3447ad396aacee496c64dbf8e9457367c2313797
Artem <sakurato456@gmail.com>
2022-02-05 16:18:57 +0300

    создал папки для проекта и раскидал их

---
+++ diplom_project/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { BasketPageComponent } from './pages/basket-page/basket-page.component';
+import { NewsPageComponent } from './pages/news-page/news-page.component';
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    ProductsPageComponent,
+    BasketPageComponent,
+    NewsPageComponent,
+    RegistrationPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

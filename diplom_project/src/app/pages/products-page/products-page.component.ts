9c64c0a6ab54fbced0af909231ed0611cf885861
Artem <sakurato456@gmail.com>
2022-04-11 21:11:25 +0300

    add json

--- diplom_project/src/app/pages/products-page/products-page.component.ts
+++ diplom_project/src/app/pages/products-page/products-page.component.ts
@@ -1,18 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { PRODUCT_ITEMS } from 'src/assets/const/product-items';
+import { Product } from 'src/assets/interfaces/products/product-item';
+import { ProductsService } from '../services/products-service';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.scss']
 })
-export class ProductsPageComponent implements OnInit {
+export class ProductsPageComponent implements OnInit ,  OnDestroy {
 
   productItems = PRODUCT_ITEMS;
 
-  constructor() { }
+  destroy$ = new Subject<void>();
+
+  itemsArray: Product[] = [];
+
+  constructor(private productsApi: ProductsService) { }
 
   ngOnInit(): void {
-  }
+    this.viewProducts()
+    }
 
+    ngOnDestroy(): any {
+      this.destroy$.next();
+      this.destroy$.complete();
+    }
+
+    private viewProducts(): void{
+      this.productsApi.getProducts().subscribe((data)=>{
+        this.itemsArray = data
+      })
+      // this.productsApi.getProducts().pipe(takeUntil(this.ngOnDestroy())).subscribe((data)=>{
+      //   this.itemsArray = data
+      // })
+      }
 }
--- diplom_project/src/app/pages/services/products-service.ts
+++ diplom_project/src/app/pages/services/products-service.ts
@@ -1,31 +1,50 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, OnDestroy } from '@angular/core';
 import { Observable, of, Subject, takeUntil } from 'rxjs';
+import { Product } from 'src/assets/interfaces/products/product-item';
 
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    Authorization: 'my-auth-token'
+  })
+};
 @Injectable({
   providedIn: 'root'
 })
-export class ProductsService implements OnDestroy {
 
-  private controller = 'localhost:3000/conditioners';
+export class ProductsService {
 
-  private $destroy = Subject
+  private controller = 'http://localhost:3000/conditioners';
 
-  item:any;
+  // private $destroy = new Subject<void>();
+
+
+  item:Product[] = [];
 
   constructor(private http: HttpClient) { }
 
+
   getProducts(){
-    this.http.get(this.controller).pipe().subscribe((s)=>{
-    
-    })
+     return this.http.get<Product[]>(this.controller);
   }
 
-  addProducts(){
-    this.http.post(this.controller, this.item).subscribe((s)=>{
-
-    })
+  addProducts(item: Product){
+    return this.http.post<Product>(this.controller, item , httpOptions)
   }
+
+  deleteProducts(id:number): Observable<unknown>{
+    const url = `${this.controller}/${id}`;
+    return this.http.delete(url, httpOptions)
+  }
+
+  updateProduct(item:Product): Observable<Product>{
+    return this.http.put<Product>(this.controller, item, httpOptions);
+  }
+
+
+
   
 
 }
